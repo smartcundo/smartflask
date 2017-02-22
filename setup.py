b88432ba9f7af85159fbdeb66bd19c346984012a
cundo <cundo@hotmail.com>
2017-02-22 14:27:34 -0800

    Created simply initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='smart',
+    version='0.0.1',
+    packages=['formula-smartflask'],
+    install_requires=[],
+    url='https://github.com/smartcundo/smartflask',
+    license='',
+    author='SMART',
+    author_email='fnishiwaki@smarttech.com',
+    description='Command and subcommand line tools for a fun DevOps World.'
+)
